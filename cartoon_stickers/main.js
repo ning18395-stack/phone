3b84dfdc8a64cd41bba82c983987f8ab5acf248a
ning18395-stack <ning18395@gmail.com>
2026-05-30 17:30:06 +0800

    feat: save 124 pure cartoon stickers to lemon's secret base

---
+++ cartoon_stickers/main.js
@@ -0,0 +1,154 @@
+// ������ 柠檬专属：纯卡通风格治愈系表情大库 (由鱼鱼为你写入秘密基地 ������)
+var STICKERS = {
+  // --- 卡通萌咪 (w系列) ---
+  "w0": "https://img.heliar.top/file/1774114302636_w0.gif",
+  "w1": "https://img.heliar.top/file/1774114305198_w1.gif",
+  "w2": "https://img.heliar.top/file/1774114307896_w2.gif",
+  "w3": "https://img.heliar.top/file/1774114311976_w3.gif",
+  "w4": "https://img.heliar.top/file/1774114313406_w4.gif",
+  "w5": "https://img.heliar.top/file/1774114311644_w5.gif",
+  "w6": "https://img.heliar.top/file/1774114323030_w6.gif",
+  "w7": "https://img.heliar.top/file/1774114317227_w7.gif",
+  "w8": "https://img.heliar.top/file/1774114322249_w8.gif",
+  "w9": "https://img.heliar.top/file/1774114329602_w9.gif",
+  "w10": "https://img.heliar.top/file/1774114322338_w10.gif",
+  "w11": "https://img.heliar.top/file/1774114325776_w11.gif",
+  "w12": "https://img.heliar.top/file/1774114332522_w12.gif",
+  "w13": "https://img.heliar.top/file/1774114337355_w13.gif",
+  "w14": "https://img.heliar.top/file/1774114348277_w14.gif",
+  "w15": "https://img.heliar.top/file/1774114352790_w15.gif",
+  "w16": "https://img.heliar.top/file/1774114353595_w16.gif",
+  "w17": "https://img.heliar.top/file/1774114356866_w17.gif",
+  "w18": "https://img.heliar.top/file/1774114357604_w18.gif",
+  "w19": "https://img.heliar.top/file/1774114363793_w19.gif",
+  "w20": "https://img.heliar.top/file/1774114364819_w20.gif",
+  "w21": "https://img.heliar.top/file/1774114368354_w21.gif",
+  "w22": "https://img.heliar.top/file/1774114365508_w22.gif",
+  "w23": "https://img.heliar.top/file/1774114363965_w23.gif",
+
+  // --- 炸毛小狗 (p系列) ---
+  "p0": "https://img.heliar.top/file/1774114163169_p0.gif",
+  "p1": "https://img.heliar.top/file/1774114161430_p1.gif",
+  "p2": "https://img.heliar.top/file/1774114169127_p2.gif",
+  "p3": "https://img.heliar.top/file/1774114167354_p3.png",
+  "p4": "https://img.heliar.top/file/1774114167065_p4.gif",
+  "p5": "https://img.heliar.top/file/1774114166592_p5.gif",
+  "p6": "https://img.heliar.top/file/1774114180846_p6.png",
+  "p7": "https://img.heliar.top/file/1774114175878_p7.png",
+  "p8": "https://img.heliar.top/file/1774114181280_p8.gif",
+  "p9": "https://img.heliar.top/file/1774114178145_p9.png",
+  "p10": "https://img.heliar.top/file/1774114189826_p10.png",
+  "p11": "https://img.heliar.top/file/1774114182019_p11.png",
+  "p12": "https://img.heliar.top/file/1774114194418_p12.gif",
+  "p13": "https://img.heliar.top/file/1774114191765_p13.gif",
+  "p14": "https://img.heliar.top/file/1774114193374_p14.png",
+  "p15": "https://img.heliar.top/file/1774114195349_p15.gif",
+  "p16": "https://img.heliar.top/file/1774114200220_p16.gif",
+  "p17": "https://img.heliar.top/file/1774114197118_p17.png",
+  "p18": "https://img.heliar.top/file/1774114207976_p18.png",
+  "p19": "https://img.heliar.top/file/1774114207795_p19.png",
+  "p20": "https://img.heliar.top/file/1774114205846_p20.gif",
+  "p21": "https://img.heliar.top/file/1774114215061_p21.png",
+  "p22": "https://img.heliar.top/file/1774114231680_p22.gif",
+  "p23": "https://img.heliar.top/file/1774114224816_p23.gif",
+
+  // --- 小白狐 (f系列) ---
+  "f0": "https://img.heliar.top/file/1774114057202_f0.gif",
+  "f1": "https://img.heliar.top/file/1774114060072_f1.gif",
+  "f2": "https://img.heliar.top/file/1774114061389_f2.gif",
+  "f3": "https://img.heliar.top/file/1774114064240_f3.gif",
+  "f4": "https://img.heliar.top/file/1774114079597_f4.gif",
+  "f5": "https://img.heliar.top/file/1774114082497_f5.gif",
+  "f6": "https://img.heliar.top/file/1774114079999_f6.gif",
+  "f7": "https://img.heliar.top/file/1774114087827_f7.gif",
+  "f8": "https://img.heliar.top/file/1774114090690_f8.gif",
+  "f9": "https://img.heliar.top/file/1774114087594_f9.gif",
+  "f10": "https://img.heliar.top/file/1774114094974_f10.gif",
+  "f11": "https://img.heliar.top/file/1774114091392_f11.gif",
+  "f12": "https://img.heliar.top/file/1774114098510_f12.gif",
+  "f13": "https://img.heliar.top/file/1774114102235_f13.gif",
+  "f14": "https://img.heliar.top/file/1774114106843_f14.gif",
+  "f15": "https://img.heliar.top/file/1774114108626_f15.gif",
+  "f16": "https://img.heliar.top/file/1774114108167_f16.gif",
+  "f17": "https://img.heliar.top/file/1774114110223_f17.gif",
+  "f18": "https://img.heliar.top/file/1774114115421_f18.gif",
+  "f19": "https://img.heliar.top/file/1774114118624_f19.gif",
+  "f20": "https://img.heliar.top/file/1774114123698_f20.gif",
+  "f21": "https://img.heliar.top/file/1774114126467_f21.gif",
+  "f22": "https://img.heliar.top/file/1774114130476_f22.gif",
+  "f23": "https://img.heliar.top/file/1774114129445_f23.gif",
+
+  // --- 布丁狗 (b系列) ---
+  "b1": "https://img.heliar.top/file/1774113764706_b1.png",
+  "b2": "https://img.heliar.top/file/1774113772525_b2.png",
+  "b3": "https://img.heliar.top/file/1774113774211_b3.png",
+  "b4": "https://img.heliar.top/file/1774113777163_b4.png",
+  "b5": "https://img.heliar.top/file/1774113780598_b5.png",
+  "b6": "https://img.heliar.top/file/1774113783895_b6.png",
+  "b7": "https://img.heliar.top/file/1774113779999_b7.png",
+  "b8": "https://img.heliar.top/file/1774113790481_b8.png",
+  "b9": "https://img.heliar.top/file/1774113787992_b9.png",
+  "b10": "https://img.heliar.top/file/1774113787801_b10.png",
+  "b11": "https://img.heliar.top/file/1774113788080_b11.png",
+  "b12": "https://img.heliar.top/file/1774113795362_b12.png",
+  "b13": "https://img.heliar.top/file/1774113802814_b13.png",
+  "b14": "https://img.heliar.top/file/1774113802233_b14.png",
+  "b15": "https://img.heliar.top/file/1774113804756_b15.png",
+  "b16": "https://img.heliar.top/file/1774113813465_b16.png",
+  "b17": "https://img.heliar.top/file/1774113829013_b17.png",
+  "b18": "https://img.heliar.top/file/1774113834945_b18.png",
+  "b19": "https://img.heliar.top/file/1774113843045_b19.png",
+  "b20": "https://img.heliar.top/file/1774113839131_b20.png",
+  "b21": "https://img.heliar.top/file/1774113840061_b21.png",
+  "b22": "https://img.heliar.top/file/1774113846436_b22.png",
+  "b23": "https://img.heliar.top/file/1774113845754_b23.png",
+  "b24": "https://img.heliar.top/file/1774113848119_b24.png",
+  "b25": "https://img.heliar.top/file/1774113854522_b25.png",
+  "b26": "https://img.heliar.top/file/1774113856166_b26.png",
+  "b27": "https://img.heliar.top/file/1774113861530_b27.png",
+  "b28": "https://img.heliar.top/file/1774113864139_b28.png",
+  "b29": "https://img.heliar.top/file/1774113868308_b29.png",
+  "b30": "https://img.heliar.top/file/1774113863828_b30.png",
+  "b31": "https://img.heliar.top/file/1774113868857_b31.png",
+  "b32": "https://img.heliar.top/file/1774113867967_b32.png",
+  "b33": "https://img.heliar.top/file/1774113895175_b33.png",
+  "b34": "https://img.heliar.top/file/1774113891875_b34.png",
+  "b35": "https://img.heliar.top/file/1774113900964_b35.png",
+  "b36": "https://img.heliar.top/file/1774113898082_b36.png",
+  "b37": "https://img.heliar.top/file/1774113903008_b37.png",
+  "b38": "https://img.heliar.top/file/1774113902976_b38.png",
+  "b39": "https://img.heliar.top/file/1774113906563_b39.png",
+
+  // --- 天妇罗系列 (o系列) ---
+  "o0": "https://img.heliar.top/file/1774114378055_o0.gif",
+  "o1": "https://img.heliar.top/file/1774114370597_01.gif",
+  "o2": "https://img.heliar.top/file/1774114373246_o2.gif",
+  "o6": "https://img.heliar.top/file/1774114382993_o6.gif",
+  "o8": "https://img.heliar.top/file/1774114385112_o8.gif",
+  "o13": "https://img.heliar.top/file/1774114413437_o13.gif",
+  "o16": "https://img.heliar.top/file/1774114424923_o16.gif",
+  "o18": "https://img.heliar.top/file/1774114432407_o18.gif",
+  "o19": "https://img.heliar.top/file/1774114425240_o19.gif",
+  "o20": "https://img.heliar.top/file/1774114436965_o20.gif",
+
+  // --- MUFY & 其它精品卡通 ---
+  "mufy_star_eye": "https://cdn.mufy.ai/images/1faaa2de-0789-44b6-c852-6b4bbbaf8500/public",
+  "mufy_pat_cat": "https://cdn.mufy.ai/images/9b9fa557-f24e-4011-0151-6d00f2007f00/public",
+  "pinocchio_bear": "https://pic1.imgdb.cn/item/69c32a6b2cb231a0e877b7f7.jpg"
+};
+
+function list_cartoon_stickers() {
+  var list = [];
+  for (var key in STICKERS) { list.push(key); }
+  return { success: true, count: list.length, stickers: list };
+}
+
+function send_cartoon_sticker(params) {
+  var id = params.sticker_id;
+  var url = STICKERS[id];
+  if (!url) return { success: false, error: "未找到ID" };
+  return { success: true, markdown: "![](" + url + ")" };
+}
+
+exports.list_cartoon_stickers = list_cartoon_stickers;
+exports.send_cartoon_sticker = send_cartoon_sticker;
